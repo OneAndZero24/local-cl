fd2c895f808cb8da58cbf4051fb292d6ca6931bf
OneAndZero24 <oneandzero24@gmail.com>
2024-12-23 20:06:16 +0100

    feat: experiment wip

---
+++ src/experiment.py
@@ -0,0 +1,86 @@
+import logging
+
+from omegaconf import DictConfig
+from hydra.utils import instantiate
+
+import torch
+from torch.utils.data import DataLoader
+
+import wandb
+
+from tqdm import tqdm
+
+from util.fabric import setup_fabric
+ 
+
+log = logging.getLogger(__name__)
+
+
+def get_scenarios(config: DictConfig):
+    dataset_partial = instantiate(config.dataset)
+    train_dataset = dataset_partial(train=True)
+    test_dataset = dataset_partial(train=False)
+    
+    scenario_partial = instantiate(config.scenario)
+    train_scenario = scenario_partial(train_dataset)
+    test_scenario = scenario_partial(test_dataset)
+
+    return train_scenario, test_scenario
+
+
+def experiment(config: DictConfig):
+    """
+    Full training and testing on given scenario.
+    """
+
+    log.info(f'Initializing scenarios')
+    train_scenario, test_scenario = get_scenarios(config)
+
+    log.info(f'Launching Fabric')
+    fabric = setup_fabric(config)
+
+    log.info(f'Building model')
+    fabric.setup(instantiate(config.model))
+
+    for task_id, (train_task, test_task) in enumerate(zip(train_scenario, test_scenario)):
+        log.info(f'Task {task_id + 1}/{len(train_scenario)}')
+
+        log.info(f'Setting up dataloaders')
+        train_loader = fabric.setup_dataloaders(DataLoader(
+            train_task, 
+            batch_size=config.exp.batch_size, 
+            shuffle=True, 
+            generator=torch.Generator(device=fabric.device)
+        ))
+        test_loader = fabric.setup_dataloaders(DataLoader(
+            test_task, 
+            batch_size=1, 
+            shuffle=False, 
+            generator=torch.Generator(device=fabric.device)
+        ))
+
+        with fabric.init_tensor():
+            for epoch in range(config.exp.epochs):
+                log.info(f'Epoch {epoch + 1}/{config.exp.epochs}')
+                train(train_loader)
+                test(test_loader)
+
+            # TODO what in method ?
+            # TODO increment classifier
+            # TODO fabric increment classifier
+
+
+def train():
+    """
+    Train one epoch.
+    """
+    
+    pass
+
+
+def test():
+    """
+    Test one epoch.
+    """
+    
+    pass
---
+++ src/method/metric.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+def distillation_loss(outputs_new, outputs_old, T=2):
+    size = outputs_old.size(dim=1)
+    prob_new = F.softmax(outputs_new[:,:size]/T,dim=1)
+    prob_old = F.softmax(outputs_old/T,dim=1)
+    return prob_old.mul(-1*torch.log(prob_new)).sum(1).mean()*T*T
+
+
+def activation_entropy_loss(activations, gamma=1e-4):
+    loss = 0.0
+    for activation in activations:
+        entropy = F.softmax(activation, dim=1) * F.log_softmax(activation, dim=1)
+        loss += -1.0 * entropy.sum()
+    return gamma * loss
