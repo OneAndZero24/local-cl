5b54fe2193bb86e81e6f9e45c5ee7ef1fde9d80b
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2024-12-28 12:30:40 +0100

    Update local layer

--- src/model/layer/local.py
+++ src/model/layer/local.py
@@ -9,42 +9,73 @@ class LocalLayer(nn.Module):
     """
 
     def __init__(self, 
-        in_features: int, 
+        in_features: int,
+        out_features: int,
         train_domain: bool = True,
         x_min: float = -1.0,
         x_max: float = 1.0,
-        device = None, 
         dtype = None
     ):
-        factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
 
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
         self.in_features = in_features
+        self.out_features = out_features
+        self.train_domain = train_domain
+
         self.x_min = x_min
         self.x_max = x_max
+
+        # Generate grid and corresponding parameters
+        domain = torch.linspace(x_min, x_max, out_features+1)
+        left_bounds  = domain[:-1].clone().unsqueeze(0).repeat(in_features,1)
+        right_bounds = domain[1:].clone().unsqueeze(0).repeat(in_features,1)
         
-        self.left_bounds = nn.Parameter(torch.empty((in_features, in_features), **factory_kwargs), requires_grad=train_domain)
-        self.right_bounds = nn.Parameter(torch.empty((in_features, in_features), **factory_kwargs), requires_grad=train_domain)
+        # Learnable parameters
+        self.left_bounds = nn.Parameter(data=left_bounds, requires_grad=train_domain).to(self.device)
+        self.right_bounds = nn.Parameter(data=right_bounds, requires_grad=train_domain).to(self.device)
+        self.linear = nn.Linear(in_features, in_features).to(self.device)
+
         self.reset_parameters()
 
     def reset_parameters(self):
-        domain = torch.linspace(self.x_min, self.x_max, self.in_features+1)
-        self.left_bounds.data = domain[:-1].clone().unsqueeze(0).repeat(self.in_features, 1)
-        self.right_bounds.data = domain[1:].clone().unsqueeze(0).repeat(self.in_features, 1)
+        nn.init.xavier_normal_(self.linear.weight)
+        nn.init.zeros_(self.linear.bias)
 
     def forward(self, x):
-        x = x.unsqueeze(2).repeat(1, 1, self.in_features)
-       
-        norm_const = 4 / (self.right_bounds - self.left_bounds)**2
 
-        x = (torch.relu(F.hardtanh(x - self.left_bounds, min_val=self.x_min, max_val=self.x_max)) \
-            * torch.relu(F.hardtanh(self.right_bounds - x, min_val=self.x_min, max_val=self.x_max)) \
-            * norm_const)**2
+        assert len(x.shape) == 2, "Please check dimensions!"
 
-        return x.sum(dim=2)
+        # Flatten input
+        x = x.flatten(start_dim=1)
+        x = self.linear(x)
+
+        upper_bound = float(torch.max(self.right_bounds))
+        lower_bound = float(torch.min(self.left_bounds))
+
+        # To project x onto domain of the hills
+        x = F.hardtanh(x, min_val=lower_bound, max_val=upper_bound)
+
+        # For stability
+        eps = 1e-4 if self.train_domain else 0.0
+
+        x = x.unsqueeze(2).repeat(1,1,self.out_features)
+        norm_const = 4 / (self.right_bounds - self.left_bounds + eps)**2
+        
+        # Forward pass
+        x = torch.relu(x - self.left_bounds) \
+            * torch.relu(self.right_bounds - x) \
+            * norm_const
+        
+        x = x * x
+        x = x.sum(dim=1)      
+
+        return x
 
     def extra_repr(self):
         return (f"in_features={self.in_features}, "
+            f"out_features={self.out_features}, " 
             f"x_min={self.x_min}, x_max={self.x_max}, "
             f"train_domain={self.left_bounds.requires_grad}")
     
