12d5d9ab8c21f3afd1b9f1817cc74fb5091cd130
OneAndZero24 <oneandzero24@gmail.com>
2025-02-25 20:06:10 +0100

    chore: joint methods grid

---
+++ scripts/two/sharpening/sharpening_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_mnist_two_sharpening_rbf
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/two/sharpening/sharpening_gs"
