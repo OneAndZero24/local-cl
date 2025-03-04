818944c207970517213811b785fa34790e889831
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-03-04 18:33:14 +0100

    Regularize feature drift in the penultimate layer

--- src/method/__init__.py
+++ src/method/__init__.py
@@ -5,3 +5,4 @@ from .mas import MAS
 from .si import SI
 from .sharpening import Sharpening
 from .reg_rbf_neuron_outputs import RBFNeuronOutReg
+from .penultimate_layer_reg import PenultimateLayerReg
