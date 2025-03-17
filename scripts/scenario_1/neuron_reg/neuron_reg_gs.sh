cf7021043a5dfa55ab64155b3602433e206c729d
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-03-17 14:51:15 +0100

    Add the first scenario

---
+++ scripts/scenario_1/neuron_reg/neuron_reg_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_mnist_neuron_reg_scenario_1
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/rbf/neuron_reg/neuron_reg_gs"
