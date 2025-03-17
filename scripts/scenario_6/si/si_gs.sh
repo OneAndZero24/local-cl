613aa9f8516c62bf976047654a17c138a5e6d650
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-03-17 17:32:51 +0100

    Fix SI in scenario 6

--- scripts/scenario_6/si/si_gs.sh
+++ scripts/scenario_6/si/si_gs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=local_cl_split_mnist_si_scenario_3
+#SBATCH --job-name=local_cl_split_mnist_si_scenario_6
 #SBATCH --qos=big
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
@@ -8,4 +8,4 @@
 
 source scripts/main.sh
 
-run_sweep_and_agent "scripts/scenario_3/si/si_gs"
+run_sweep_and_agent "scripts/scenario_6/si/si_gs"
