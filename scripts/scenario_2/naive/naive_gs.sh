e64dbf8e1ff19f57205bf2ee7be2419524c4e6f8
OneAndZero24 <oneandzero24@gmail.com>
2025-03-27 15:39:59 +0100

    CONFIGS

---
+++ scripts/scenario_2/naive/naive_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_mnist_naive_scenario_2
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/scenario_2/naive/naive_gs"
