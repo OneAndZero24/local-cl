5237f9172e38565f5bb7a6e5696acde70698a155
Jan Miksa <oneandzero24@gmail.com>
2025-05-01 19:33:03 +0200

    CONFIGS FOR ResNet18/ViT CoRALS GRIDS (#28)
    * chore: timm models + cleanup
    
    * chore: configs dyn alpha
    
    * chore: configs no dyn alpha
    
    * chore: naive
    
    * Update scripts
    
    * Fix initial number of classes per task
    
    * Fix no tasks in each dataset
    
    ---------
    
    Co-authored-by: Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>

---
+++ scripts/dynamic_alpha/cifar100/vit/ewc/ewc_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_cifar100_vit_ewc_dynamic_alpha
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/dynamic_alpha/cifar100/vit/ewc/ewc_gs"
---
+++ scripts/no_dynamic_alpha/imagenet/resnet18/naive/naive_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_imagenet_resnet_naive_dynamic_alpha
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/no_dynamic_alpha/imagenet/resnet18/naive/naive_gs"
