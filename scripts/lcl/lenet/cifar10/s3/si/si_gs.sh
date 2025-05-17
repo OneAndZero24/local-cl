219c51c731e6a1e7253a6ebe4e0891a5ca83bf5d
OneAndZero24 <oneandzero24@gmail.com>
2025-05-17 22:08:08 +0200

    configs

---
+++ scripts/lcl/lenet/cifar10/s3/si/si_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_cifar10_lenet_s3_si
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/lcl/lenet/cifar10/s3/si/si_gs"
---
+++ scripts/lcl/lenet/svhn/s4/mas/mas_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_svhn_lenet_s4_mas
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/lcl/lenet/svhn/s4/mas/mas_gs"
