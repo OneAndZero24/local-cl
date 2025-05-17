d6d364a751d5e7902bb8fa96bf732c8cc3949b1b
OneAndZero24 <oneandzero24@gmail.com>
2025-05-17 22:31:06 +0200

    lwf

---
+++ scripts/lcl/resnet18/cifar10/s3/lwf/lwf_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_cifar10_resnet18_s3_lwf
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/lcl/resnet18/cifar10/s3/lwf/lwf_gs"
