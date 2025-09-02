6a63bc994ffd5a002cbdd07e68a3c063d9a0ae4d
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-09-02 13:10:14 +0200

    Work in progress

--- src/method/interval_penalization.py
+++ src/method/interval_penalization.py
@@ -1,11 +1,11 @@
 import logging
 from copy import deepcopy
 from typing import Tuple
+from collections import OrderedDict
 
 import torch
 
 from src.method.method_plugin_abc import MethodPluginABC
-
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -18,6 +18,7 @@ class IntervalPenalization(MethodPluginABC):
     Attributes:
         var_scale (float): Weight for the variance term.
         output_reg_scale (float): Weight for output preservation.
+        interval_drift_reg_scale (float): Weight for interval representation drift.
         task_id (int or None): Current task identifier.
         params_buffer (dict): Optimal parameters from the last task.
 
@@ -30,7 +31,8 @@ class IntervalPenalization(MethodPluginABC):
 
     def __init__(self,
             var_scale: float = 0.01,
-            output_reg_scale: float = 1.0
+            output_reg_scale: float = 1.0,
+            interval_drift_reg_scale: float = 1.0
         ):
         """
         Initializes the IntervalPenalization plugin.
@@ -38,6 +40,7 @@ class IntervalPenalization(MethodPluginABC):
         Args:
             var_scale (optional, float): Weight for output preservation.
             output_reg_scale (optional, float): Weight for output preservation.
+            interval_drift_reg_scale (optional, float): Weight for interval representation drift.
 
         """
         
@@ -47,8 +50,49 @@ class IntervalPenalization(MethodPluginABC):
 
         self.var_scale = var_scale
         self.output_reg_scale = output_reg_scale
+        self.interval_drift_reg_scale = interval_drift_reg_scale
+
         self.input_shape = None
         self.params_buffer = {}
+
+    def forward_with_snapshot(self, x, stop_at="IntervalActivation"):
+        """
+        Run the model forward using frozen params/buffers, stopping at the first IntervalActivation.
+        """
+        # Save references to current parameter data and buffer tensors
+        saved_param_datas = {name: param.data for name, param in self.module.named_parameters()}
+        saved_buffers = {name: buf for name, buf in self.module.named_buffers()}
+
+        # Set parameters to snapshot values (using clones to avoid inplace on originals)
+        for name, param in self.module.named_parameters():
+            param.data = self.old_state["params"][name].clone()
+        
+        # Set buffers to snapshot values (using clones)
+        for name, buf in self.module.named_buffers():
+            self.module._buffers[name] = self.old_state["buffers"][name].clone()
+
+        # Run the forward pass
+        out = x
+        for layer in self.module.layers:
+            out = layer(out)
+            if type(layer).__name__ == stop_at:
+                break
+
+        # Restore original parameter data and buffer tensors
+        for name, param in self.module.named_parameters():
+            param.data = saved_param_datas[name]
+        
+        for name, buf in self.module.named_buffers():
+            self.module._buffers[name] = saved_buffers[name]
+
+        return out
+
+    @torch.no_grad()
+    def snapshot_state(self):
+        return {
+            "params": OrderedDict((k, v.detach().clone()) for k, v in self.module.named_parameters()),
+            "buffers": OrderedDict((k, v.detach().clone()) for k, v in self.module.named_buffers()),
+        }
 
 
     def setup_task(self, task_id: int):
@@ -60,13 +104,14 @@ class IntervalPenalization(MethodPluginABC):
         """
 
         self.task_id = task_id
-
         if task_id > 0:
+            self.params_buffer = {}
             for name, p in deepcopy(list(self.module.named_parameters())):
                 if p.requires_grad:
                     p.requires_grad = False
-                    self.params_buffer[name] = p 
-            
+                    self.params_buffer[name] = p.detach().clone()
+            self.old_state = self.snapshot_state()
+                
             # To debug: If those layers are frozen, BUT
             # a classification head is unfrozen, then we have
             # zero forgetting!
@@ -90,11 +135,14 @@ class IntervalPenalization(MethodPluginABC):
             tuple: (loss, preds) with penalization added to loss.
         """
 
-        self.input_shape = x.flatten(start_dim=1).shape
+        x = x.flatten(start_dim=1)
+        self.input_shape = x.shape
 
         layers = self.module.layers + [self.module.head]
+
         var_loss = 0.0
         output_reg_loss = 0.0
+        interval_drift_loss = 0.0
 
         for idx, layer in enumerate(layers):
             if not type(layer).__name__ == "IntervalActivation":
@@ -106,9 +154,20 @@ class IntervalPenalization(MethodPluginABC):
             var_loss += batch_var
 
             if self.task_id > 0:
+
                 lb = layer.min
                 ub = layer.max
 
+                middle = (lb + ub) / 2
+                r = (ub - lb) / 2
+
+                y_old = self.forward_with_snapshot(x)
+                mask = ((acts >= lb) & (acts <= ub))
+                drift_weight = torch.exp(-((acts - middle) ** 2) / r) * mask
+
+                interval_drift_loss = interval_drift_loss + (drift_weight * (y_old.detach() - acts).pow(2)).mean()
+
+                # Regularization of learnable parameters above the IntervalActivation layer
                 next_layer = layers[idx + 1]
                 if hasattr(next_layer, "classifier"):
                     lower_bound_reg = 0.0
@@ -116,7 +175,7 @@ class IntervalPenalization(MethodPluginABC):
                     for name, p in next_layer.classifier.named_parameters():
                         for mod_name, mod_param in self.module.named_parameters():
                             if mod_param is p and mod_name in self.params_buffer:
-                                prev_param = self.params_buffer[mod_name].to(p.device)
+                                prev_param = self.params_buffer[mod_name]
                                 if "weight" in name:
                                     weight_diff = p - prev_param
 
@@ -132,5 +191,8 @@ class IntervalPenalization(MethodPluginABC):
 
                 output_reg_loss += lower_bound_reg.sum().pow(2) + upper_bound_reg.sum().pow(2)
     
-        loss = loss + self.var_scale * var_loss + self.output_reg_scale * output_reg_loss
+        loss = loss \
+            + self.var_scale * var_loss \
+                + self.output_reg_scale * output_reg_loss \
+                + self.interval_drift_reg_scale * interval_drift_loss
         return loss, preds
