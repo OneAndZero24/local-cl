de777fba7f38034b9f952bc703745be3e6eb9e41
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-09-23 15:57:40 +0200

    Add experiments

---
+++ scripts/interval_activation/mnist/reg_whole_cls_outputs_min_var/reg_whole_cls_outputs_min_var_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=local_cl_split_mnist_mlp_reg_whole_cls_outputs_min_var
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/interval_activation/mnist/mlp/reg_whole_cls_outputs_min_var_gs"
